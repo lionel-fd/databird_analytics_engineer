07e086bc983ac0e4f4ac39966484756f7b6c177b
Fruchard Lionel <amphybioz@gmail.com>
2025-12-28 13:37:17 +0000

    fix: product granularity

--- models/intermediate/int_local_bike__orders_shipped.md
+++ models/intermediate/int_local_bike__orders_shipped.md
@@ -1,11 +1,8 @@
 {% docs int_local_bike__orders_shipped %}
 
-This model provides an aggregated view of orders, combining data with order items and add measures:
+This model provides a sales base view, combining data with order items and add measures:
 
-- **total_distinct_products**: sum of different products for each order
-- **total_quantity_items**: sum of quantity of products for each order
-- **brut_total_amount**: sum of all amounts before discount for each order
-- **total_discount_amount**: sum of discount amounts 
-- **total_net_amount**: sum of all amounts including discount
+- **delivery_delay_days**: days number between required_date_at and shipped_date_at
+- **delivery_status**: <0 "ahead", =0 "on_time" >0 "late"
 
 {% enddocs %}
--- models/intermediate/int_local_bike__orders_shipped.sql
+++ models/intermediate/int_local_bike__orders_shipped.sql
@@ -1,34 +1,25 @@
-with orders_grouped as
+with sales_base as
 (
 select 
+    oi.product_id,
     o.order_id,
     o.order_status_code,
     o.order_date_at,
     o.required_date_at,
     o.shipped_date_at,
+    date_diff(o.shipped_date_at,o.required_date_at, DAY) as delivery_delay_days,
     o.store_id,
     o.customer_id,
-    
-    count(distinct oi.product_id) as total_distinct_products,
-    sum(brut_total_amount) as brut_total_amount,
-    sum(oi.quantity) as total_quantity_items,
-    sum(oi.discount_amount) as total_discount_amount,
-    sum(oi.net_amount) as total_net_amount,
-    date_diff(o.shipped_date_at,o.required_date_at, DAY) as delivery_delay_days,
+    oi.brut_total_amount,
+    oi.quantity,
+    oi.discount_amount,
+    oi.net_amount
     
 from {{ ref('int_local_bike__order_items') }} oi
-join {{ ref('stg_local_bike__orders')}} o
+join {{ ref('stg_local_bike__orders') }} o
 on oi.order_id = o.order_id
 
 where o.order_status_code = 4
-group by 
-    o.order_id,
-    o.order_date_at,
-    o.required_date_at,
-    o.shipped_date_at,
-    o.store_id,
-    o.customer_id,
-    o.order_status_code
 ),
 
 final as 
@@ -39,7 +30,7 @@ final as
         when s.delivery_delay_days = 0 then 'on_time'
         else 'late'
      end as delivery_status
-    from orders_grouped as s
+    from sales_base as s
 )
 
 select *

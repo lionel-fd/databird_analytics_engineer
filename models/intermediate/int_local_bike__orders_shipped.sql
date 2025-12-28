de5610c6626e2977b3b048a2939517a8c78beda9
Fruchard Lionel <amphybioz@gmail.com>
2025-12-28 14:59:17 +0000

    update: stock localisation and add store_city information in orders_shipped

--- models/intermediate/int_local_bike__orders_shipped.sql
+++ models/intermediate/int_local_bike__orders_shipped.sql
@@ -9,6 +9,7 @@ select
     o.shipped_date_at,
     date_diff(o.shipped_date_at,o.required_date_at, DAY) as delivery_delay_days,
     o.store_id,
+    s.city,
     o.customer_id,
     oi.brut_total_amount,
     oi.quantity,
@@ -18,6 +19,8 @@ select
 from {{ ref('int_local_bike__order_items') }} oi
 join {{ ref('stg_local_bike__orders') }} o
 on oi.order_id = o.order_id
+join {{ ref('stg_local_bike__stores') }} s
+on o.store_id = s.store_id
 
 where o.order_status_code = 4
 ),
@@ -26,11 +29,11 @@ final as
 (
     select *,
     case 
-        when s.delivery_delay_days < 0 then 'ahead'
-        when s.delivery_delay_days = 0 then 'on_time'
+        when sb.delivery_delay_days < 0 then 'ahead'
+        when sb.delivery_delay_days = 0 then 'on_time'
         else 'late'
      end as delivery_status
-    from sales_base as s
+    from sales_base as sb
 )
 
 select *

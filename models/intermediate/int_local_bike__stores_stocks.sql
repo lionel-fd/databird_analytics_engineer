088bea33c8d874c07af8223a860094c2a3e42006
Fruchard Lionel <amphybioz@gmail.com>
2025-12-30 16:24:53 +0000

    add: positives tests in _mrt_local_bike_bi and delete columns useless in int____stores_stocks and complete dim_product

--- models/intermediate/int_local_bike__stores_stocks.sql
+++ models/intermediate/int_local_bike__stores_stocks.sql
@@ -7,31 +7,16 @@ products as
 (
     select 
         product_id,
-        product_name,
-        category_id,
-        model_year,
-        catalog_price 
+        catalog_price
     from {{ ref('stg_local_bike__products') }}
-),
-categories as
-(
-    select
-        category_id,
-        category_name
-    from {{ ref('stg_local_bike__categories') }}
 )
 
 select
     s.store_product_id,
     s.store_id,
     s.product_id,
-    p.product_name,
-    p.model_year,
-    c.category_name,
     s.quantity_in_stock,
     (s.quantity_in_stock * p.catalog_price) as stock_value
 from stocks as s
 join products as p 
     on s.product_id = p.product_id
-join categories as c
-    on p.category_id = c.category_id
--- models/mart/dim_product.sql
+++ models/mart/dim_product.sql
@@ -1,7 +1,12 @@
 select 
     product_id,
     product_name,
-    category_id,
+    category_name,
     model_year,
-    brand_id
-from {{ ref('stg_local_bike__products')}}
+    brand_name,
+    catalog_price
+from {{ ref('stg_local_bike__products')}} as p
+left join {{ ref('stg_local_bike__categories')}} as c
+    on p.category_id = c.category_id
+left join {{ ref('stg_local_bike__brands')}} as b
+    on p.brand_id = b.brand_id
